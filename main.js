2441322fd9468309538dfd52d5043d5f7436ae6a
mateo <mateolroca06@gmail.com>
2025-03-17 15:44:56 -0300

    cambios importantes en el main.js

--- main.js
+++ main.js
@@ -5,31 +5,60 @@ function IngresarUsuario() {
  if (nombreUsuario=="") {
     alert("error:usuario no encontrado");
  } else {
-    alert("Bienvenido " + nombreUsuario);
+    alert("Bienvenido a la sucursal Chevrolet " + nombreUsuario);
  }   
 
 }
 
-function pedirTurno() {
-    let solicitarTurno=prompt("¿quieres pedir un turno?")
-    if (solicitarTurno=="no") {
-        alert("ok muchas gracias vuelva pronto")
-    } 
-    if (solicitarTurno="si") {
-    
-        for (let i = 1; i <=20; i++) {
-        
-            alert("su turno es el Nro "+i+" Usuario: " + nombreUsuario )
-            break
-        
+const Auto = function (nombre,precio,año,cantidad){
+    this.nombre = nombre,
+    this.precio = precio,
+    this.año = año,
+    this.cantidad = cantidad
+}
+ let auto1= new Auto("corsa",120000,2008,5)
+ let auto2= new Auto("montana",240000,2024,20)
+ let auto3= new Auto("onix",600000,2023,8)
+ let auto4= new Auto("tracker",360000,2024,12)
+ const lista = [auto1,auto2,auto3,auto4]
+ function BuscarAuto(){
+    let palabraClave = prompt("ingresa el nombre del auto q estas buscando").trim().toUpperCase()
+    let resultado = lista.filter( (x)=> x.nombre.toUpperCase().includes(palabraClave))
+    if (resultado.length >0) {
+        console.table(resultado)
+        ComprarAuto(resultado); 
+    } else {
+       alert("no se encontro el auto que buscas") 
     }
+ }
+ function ComprarAuto(resultado){
+    let eleccion = prompt("¿desea comprar el auto elegido? si/no").toLowerCase();
+
+if (eleccion==="si") {
+
+    let nombredelAuto = prompt("ingrese el nombre del auto por favor").trim();
+    let AutoElegido = resultado.find(auto => auto.nombre.toLowerCase() === nombredelAuto.toLowerCase());
+
+    if (AutoElegido){
+        let cuotas = 12;
+        let cuotaMensual = AutoElegido.precio / cuotas; 
+        alert(`El precio del auto ${ AutoElegido.nombre } es ${ AutoElegido.precio } Cada cuota será de ${ cuotaMensual } durante ${ cuotas } meses.`);
+
+    }
+} else {
+    alert("Selección no válida.");
 }
-if (solicitarTurno=="") {
-    alert("error:no hay respuesta por parte del usuario")
+if (eleccion==="no") {
+    alert("recibido gracias por entrar a nuestra pagina")
 }
-}
+ }
+
+IngresarUsuario()   
+BuscarAuto()
+
+
+
  
-IngresarUsuario()
-pedirTurno()
+ 
 
 
